006127221c2774592a8b4f2920c7cb6b654a6f71
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-09-02 00:47:52 +0200

    Late night cleanup and refactor

--- Connect4.java
+++ Connect4.java
@@ -14,25 +14,22 @@ public class Connect4 {
 
     public static void main(String[] args) {
         Connect4JFrame frame = new Connect4JFrame();
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
 
 class Connect4JFrame extends JFrame implements ActionListener {
 
-    private Button          btn1, btn2, btn3, btn4, btn5, btn6, btn7;
-    private Label           lblSpacer;
-    MenuItem                newMI, exitMI, redMI, yellowMI;
-    int[][]                 theArray;
-    boolean                 end=false;
-    boolean                 gameStart;
-    public static final int BLANK = 0;
-    public static final int RED = 1;
-    public static final int YELLOW = 2;
-
     public static final int MAXROW = 6;     // 6 rows
     public static final int MAXCOL = 7;     // 7 columns
+
+    private Button[] btn = new Button[MAXCOL];
+    private Label lblSpacer;
+    MenuItem newMI, exitMI, redMI, yellowMI;
+    Color[][] theArray;
+    boolean end = false;
+    boolean gameStart;
 
     int redMoves = 0;
     int yellowMoves = 0;
@@ -42,10 +39,8 @@ class Connect4JFrame extends JFrame implements ActionListener {
     private Label yellowMovesLabel;
     private Label totalMovesLabel;
 
-
     public static final String SPACE = "                  "; // 18 spaces
-
-    int activeColour = RED;
+    Color activeColour = Color.RED;
 
     public Connect4JFrame() {
         setTitle("Connect4 by Chris Clarke");
@@ -58,6 +53,7 @@ class Connect4JFrame extends JFrame implements ActionListener {
         exitMI.addActionListener(this);
         fileMenu.add(exitMI);
         mbar.add(fileMenu);
+
         Menu optMenu = new Menu("Options");
         redMI = new MenuItem("Red starts");
         redMI.addActionListener(this);
@@ -68,11 +64,10 @@ class Connect4JFrame extends JFrame implements ActionListener {
         mbar.add(optMenu);
         setMenuBar(mbar);
 
-
         Panel statsPanel = new Panel();
         redMovesLabel = new Label("Red Moves: 0");
-        yellowMovesLabel = new Label("yellow Moves: 0");
-        totalMovesLabel = new Label("total Moves: 0");
+        yellowMovesLabel = new Label("Yellow Moves: 0");
+        totalMovesLabel = new Label("Total Moves: 0");
 
         statsPanel.add(redMovesLabel);
         statsPanel.add(yellowMovesLabel);
@@ -82,59 +77,26 @@ class Connect4JFrame extends JFrame implements ActionListener {
 
         // Build control panel.
         Panel panel = new Panel();
-
-        btn1 = new Button("1");
-        btn1.addActionListener(this);
-        panel.add(btn1);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn2 = new Button("2");
-        btn2.addActionListener(this);
-        panel.add(btn2);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn3 = new Button("3");
-        btn3.addActionListener(this);
-        panel.add(btn3);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn4 = new Button("4");
-        btn4.addActionListener(this);
-        panel.add(btn4);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn5 = new Button("5");
-        btn5.addActionListener(this);
-        panel.add(btn5);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn6 = new Button("6");
-        btn6.addActionListener(this);
-        panel.add(btn6);
-        lblSpacer = new Label(SPACE);
-        panel.add(lblSpacer);
-
-        btn7 = new Button("7");
-        btn7.addActionListener(this);
-        panel.add(btn7);
+        for (int i = 0; i < btn.length; i++) {
+            btn[i] = new Button("" + i);
+            btn[i].addActionListener(this);
+            panel.add(btn[i]);
+            lblSpacer = new Label(SPACE);
+            panel.add(lblSpacer);
+        }
 
         add(panel, BorderLayout.NORTH);
         initialize();
-        // Set to a reasonable size.
         setSize(1024, 768);
-    } // Connect4
+    }
 
     public void initialize() {
-        theArray=new int[MAXROW][MAXCOL];
-        for (int row=0; row<MAXROW; row++)
-            for (int col=0; col<MAXCOL; col++)
-                theArray[row][col]=BLANK;
-        gameStart=false;
+        theArray = new Color[MAXROW][MAXCOL];
+        for (int row = 0; row < MAXROW; row++)
+            for (int col = 0; col < MAXCOL; col++)
+                theArray[row][col] = Color.WHITE;
+        gameStart = false;
+        end = false;
 
         redMoves = 0;
         yellowMoves = 0;
@@ -143,37 +105,32 @@ class Connect4JFrame extends JFrame implements ActionListener {
         redMovesLabel.setText("Red Moves: 0");
         yellowMovesLabel.setText("Yellow Moves: 0");
         totalMovesLabel.setText("Total Moves: 0");
-    } // initialize
+    }
 
     public void paint(Graphics g) {
         g.setColor(Color.BLUE);
-        g.fillRect(110, 50, 100+100*MAXCOL, 100+100*MAXROW);
-        for (int row=0; row<MAXROW; row++)
-            for (int col=0; col<MAXCOL; col++) {
-                if (theArray[row][col]==BLANK) g.setColor(Color.WHITE);
-                if (theArray[row][col]==RED) g.setColor(Color.RED);
-                if (theArray[row][col]==YELLOW) g.setColor(Color.YELLOW);
-                g.fillOval(160+100*col, 100+100*row, 100, 100);
+        g.fillRect(110, 50, 100 + 100 * MAXCOL, 100 + 100 * MAXROW);
+        for (int row = 0; row < MAXROW; row++)
+            for (int col = 0; col < MAXCOL; col++) {
+                g.setColor(theArray[row][col]);
+                g.fillOval(160 + 100 * col, 100 + 100 * row, 100, 100);
             }
         check4(g);
-    } // paint
+    }
 
     public void putDisk(int n) {
-        // put a disk on top of column n
-        // if game is won, do nothing
         if (end) return;
-        gameStart=true;
+        gameStart = true;
         int row;
-        n--;
-        for (row=0; row<MAXROW; row++)
-            if (theArray[row][n]>0) break;
-        if (row>0) {
-            theArray[--row][n]=activeColour;
-            if (activeColour==RED) {
-                activeColour=YELLOW;
+        for (row = 0; row < MAXROW; row++)
+            if (theArray[row][n] != Color.WHITE) break;
+        if (row > 0) {
+            theArray[--row][n] = activeColour;
+            if (activeColour == Color.RED) {
+                activeColour = Color.YELLOW;
                 yellowMoves++;
             } else {
-                activeColour=RED;
+                activeColour = Color.RED;
                 redMoves++;
             }
             totalMoves++;
@@ -182,97 +139,93 @@ class Connect4JFrame extends JFrame implements ActionListener {
             yellowMovesLabel.setText("Yellow Moves: " + yellowMoves);
             totalMovesLabel.setText("Total Moves: " + totalMoves);
 
-
             repaint();
         }
     }
 
-    public void displayWinner(Graphics g, int n) {
+    public void displayWinner(Graphics g, Color n) {
         g.setColor(Color.BLACK);
         g.setFont(new Font("Courier", Font.BOLD, 100));
-        if (n==RED)
+        if (n == Color.RED)
             g.drawString("Red wins!", 100, 400);
         else
             g.drawString("Yellow wins!", 100, 400);
-        end=true;
+        end = true;
     }
 
     public void check4(Graphics g) {
-        // see if there are 4 disks in a row: horizontal, vertical or diagonal
-        // horizontal rows
-        for (int row=0; row<MAXROW; row++) {
-            for (int col=0; col<MAXCOL-3; col++) {
-                int curr = theArray[row][col];
-                if (curr>0
-                        && curr == theArray[row][col+1]
-                        && curr == theArray[row][col+2]
-                        && curr == theArray[row][col+3]) {
-                    displayWinner(g, theArray[row][col]);
+        // horizontal
+        for (int row = 0; row < MAXROW; row++) {
+            for (int col = 0; col < MAXCOL - 3; col++) {
+                Color curr = theArray[row][col];
+                if (curr == Color.WHITE) continue;
+                if (curr == theArray[row][col + 1] &&
+                    curr == theArray[row][col + 2] &&
+                    curr == theArray[row][col + 3]) {
+                    displayWinner(g, curr);
                 }
             }
         }
-        // vertical columns
-        for (int col=0; col<MAXCOL; col++) {
-            for (int row=0; row<MAXROW-3; row++) {
-                int curr = theArray[row][col];
-                if (curr>0
-                        && curr == theArray[row+1][col]
-                        && curr == theArray[row+2][col]
-                        && curr == theArray[row+3][col])
-                    displayWinner(g, theArray[row][col]);
+        // vertical
+        for (int col = 0; col < MAXCOL; col++) {
+            for (int row = 0; row < MAXROW - 3; row++) {
+                Color curr = theArray[row][col];
+                if (curr == Color.WHITE) continue;
+                if (curr == theArray[row + 1][col] &&
+                    curr == theArray[row + 2][col] &&
+                    curr == theArray[row + 3][col]) {
+                    displayWinner(g, curr);
+                }
             }
         }
-        // diagonal lower left to upper right
-        for (int row=0; row<MAXROW-3; row++) {
-            for (int col=0; col<MAXCOL-3; col++) {
-                int curr = theArray[row][col];
-                if (curr>0
-                        && curr == theArray[row+1][col+1]
-                        && curr == theArray[row+2][col+2]
-                        && curr == theArray[row+3][col+3])
-                    displayWinner(g, theArray[row][col]);
+        // diag down-right
+        for (int row = 0; row < MAXROW - 3; row++) {
+            for (int col = 0; col < MAXCOL - 3; col++) {
+                Color curr = theArray[row][col];
+                if (curr == Color.WHITE) continue;
+                if (curr == theArray[row + 1][col + 1] &&
+                    curr == theArray[row + 2][col + 2] &&
+                    curr == theArray[row + 3][col + 3]) {
+                    displayWinner(g, curr);
+                }
             }
         }
-        // diagonal upper left to lower right
-        for (int row=MAXROW-1; row>=3; row--) {
-            for (int col=0; col<MAXCOL-3; col++) {
-                int curr = theArray[row][col];
-                if (curr>0
-                        && curr == theArray[row-1][col+1]
-                        && curr == theArray[row-2][col+2]
-                        && curr == theArray[row-3][col+3])
-                    displayWinner(g, theArray[row][col]);
+        // diag up-right
+        for (int row = MAXROW - 1; row >= 3; row--) {
+            for (int col = 0; col < MAXCOL - 3; col++) {
+                Color curr = theArray[row][col];
+                if (curr == Color.WHITE) continue;
+                if (curr == theArray[row - 1][col + 1] &&
+                    curr == theArray[row - 2][col + 2] &&
+                    curr == theArray[row - 3][col + 3]) {
+                    displayWinner(g, curr);
+                }
             }
         }
-    } // end check4
+    }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btn1)
-            putDisk(1);
-        else if (e.getSource() == btn2)
-            putDisk(2);
-        else if (e.getSource() == btn3)
-            putDisk(3);
-        else if (e.getSource() == btn4)
-            putDisk(4);
-        else if (e.getSource() == btn5)
-            putDisk(5);
-        else if (e.getSource() == btn6)
-            putDisk(6);
-        else if (e.getSource() == btn7)
-            putDisk(7);
-        else if (e.getSource() == newMI) {
-            end=false;
+
+        for (int i = 0; i < btn.length; i++) {
+
+            if (e.getSource() == btn[i]) {
+                putDisk(i);
+                return;
+            }
+        }
+
+        if (e.getSource() == newMI) {
             initialize();
             repaint();
         } else if (e.getSource() == exitMI) {
             System.exit(0);
         } else if (e.getSource() == redMI) {
-            // don't change colour to play in middle of game
-            if (!gameStart) activeColour=RED;
+            if (!gameStart) activeColour = Color.RED;
         } else if (e.getSource() == yellowMI) {
-            if (!gameStart) activeColour=YELLOW;
-        }
-    } // end ActionPerformed
+            if (!gameStart) activeColour = Color.YELLOW;
 
-} // class
+        }
+    }
+
+}
